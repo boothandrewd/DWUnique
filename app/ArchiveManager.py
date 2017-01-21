6726b423ab8c00c7ce701b7990a3ab160b412e4b
adbooth <adbooth8@gmail.com>
2017-01-20 20:47:21 -0500

    Commented AM.py

--- app/ArchiveManager.py
+++ app/ArchiveManager.py
@@ -1,4 +1,5 @@
 """ app/ArchiveManager.py
+Class for interfacing with the database.
 """
 import os
 from datetime import date
@@ -17,6 +18,7 @@ spotify = spotipy.Spotify(client_credentials_manager=spCreds())
 
 class ArchiveManager:
     def __init__(self, playlist_id, mobile_number=None):
+        """ Initializes object and creates object in database if necessary. """
         try:
             if not is_hash(playlist_id):
                 raise ValueError
@@ -32,36 +34,43 @@ class ArchiveManager:
                 'playlist_id': self.playlistId,
                 'mobile_number': mobile_number and clean_mobile_number(mobile_number),
                 'playlists': [],
-                'playlist': []
+                'playlist': [],
+                'playlist_date': ''
             })
 
     def setNumber(self, mobile_number):
+        """ A setter for a user's mobile number. """
         archives.update_one(self.playlist_filter, {
             '$set': {'mobile_number': clean_mobile_number(mobile_number)}
         })
 
     def getNumber(self):
+        """ A getter for a user's mobile number. """
         return archives.find_one(self.playlist_filter, {
             '_id': 0,
             'mobile_number': 1
         })['mobile_number'] or None
 
     def getCurrentPlaylist(self):
+        """ Gets a list of track_ids referring to the current DWUnique. """
         return archives.find_one(self.playlist_filter, {
             '_id': 0,
             'playlist': 1
         })['playlist'] or []
 
     def getPlaylists(self):
+        """ Gets list of dated DWUniques, which are each lists of track_ids. """
         return archives.find_one(self.playlist_filter, {
             '_id': 0,
             'playlists': 1
         })['playlists'] or []
 
     def getHistory(self):
+        """ Gets a list of all previous tracks by track_id. """
         return [i for t in self.getPlaylists() for i in t[1]]
 
     def update(self):
+        """ Determines the current new playlist and updates database. """
         playlist_updated = False
 
         # Get Spotify's DW
